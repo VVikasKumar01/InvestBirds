0bdd4e819b4c2a888cd9c558f2be0b540d7bdaad
Vikas Kumar Velugubantla <165983372+VVikasKumar01@users.noreply.github.com>
2025-04-06 19:22:40 +0530

    Delete backend directory

--- backend/routes/investRoutes.js
+++
@@ -1,8 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const { buildPortfolio } = require('../controllers/investController');
-
-router.post('/build', buildPortfolio);
-
-module.exports = router;
